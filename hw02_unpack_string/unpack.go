5d4533b36f88a1c53209214d6f0a7c89f6deeb95
Иван Гусаров <i.gusarov@scieddi.ru>
2021-09-13 16:54:06 +0300

    hw 02 refactoring 3

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -4,44 +4,43 @@ import (
 	"errors"
 	"strconv"
 	"strings"
-	"unicode"
 )
+
+func isDigit(char int32) bool {
+	if char >= 48 && char <= 57 {
+		return true
+	}
+
+	return false
+}
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(name string) (string, error) {
-	linkName := &name
-
 	if name == "" {
 		return "", nil
-	} else if unicode.IsDigit(rune(name[0])) {
+	} else if isDigit(int32(name[0])) {
 		return "", ErrInvalidString
 	}
 
 	for index, val := range name {
-		if unicode.IsDigit(val) && unicode.IsDigit(rune(name[index+1])) {
+		if isDigit(val) && (isDigit(int32(name[index+1])) && index+1 != len(name)) {
 			return "", ErrInvalidString
-		} else if unicode.IsDigit(val) {
+		} else if isDigit(val) {
 			currentNumber, _ := strconv.Atoi(string(val))
 			splitedName := strings.Split(name, string(val))
 			lastChar := len(splitedName[0]) - 1
-			if currentNumber == 0 {
-				currentNumber = 1
-			}
-			repeatedChar := strings.Repeat(string(splitedName[0][lastChar]), currentNumber-1)
+
+			repeatedChar := strings.Repeat(string(splitedName[0][lastChar]), currentNumber)
 			var b strings.Builder
 
-			for i := 1; i >= 1; i-- {
-				if currentNumber == 1 {
-					b.WriteString(splitedName[0][0 : len(splitedName[0])-1])
-				} else {
-					b.WriteString(splitedName[0])
-					b.WriteString(repeatedChar)
-				}
+			b.WriteString(splitedName[0][0 : len(splitedName[0])-1])
+			if currentNumber > 1 {
+				b.WriteString(repeatedChar)
 			}
 
 			splitedName[0] = b.String()
-			*linkName = strings.Join(splitedName, "")
+			name = strings.Join(splitedName, "")
 		}
 	}
 
